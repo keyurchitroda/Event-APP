2f7fca8f1e194dffecc71dc3cf3b6e111dc1724a
Keyur Chitroda <keyurchitroda@gmail.com>
2024-12-24 14:13:35 +0530

    fix null issue

--- src/app/api/webhooks/clerk/route.ts
+++ src/app/api/webhooks/clerk/route.ts
@@ -57,9 +57,9 @@ export async function POST(req: Request) {
     const user = {
       clerkId: id,
       email: email_addresses[0].email_address,
-      username: username!,
-      firstName: first_name,
-      lastName: last_name,
+      username: username ? username : "",
+      firstName: first_name ? first_name : "",
+      lastName: last_name ? last_name : "",
       photo: image_url,
     };
 
